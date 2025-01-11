1afb0e35af312ad6a004c9a525f1b376c7a5769e
joheiber almao <joheiberalmao1214@gmail.com>
2025-01-10 19:41:15 -0500

    mejore el diseño de las card d la peliculas

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import './style.css'
 $(document).ready(() => {
 
   $("#inputBusqueda").on("input", function () {
-    // Capturar el valor del input
     const valor = $(this).val();
 
     const options = {
@@ -18,16 +17,30 @@ $(document).ready(() => {
       .then(res => res.json())
       .then(res => {
         peliculas = res.results
+        console.log(peliculas)
+
         $("#tableroPeliculas").empty()
+
         peliculas.forEach((pelicula) => {
+          let overview = pelicula.overview.length > 150
+            ? pelicula.overview.substring(0, 150) + "..."
+            : pelicula.overview;
           $("#tableroPeliculas").append(`
-            <div class="card my-1" style="width: 18rem;">
-  <img src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}" class="card-img-top" alt="...">
-  <div class="card-body">
-  <h5 class="card-title">${pelicula.title}</h5>
-    <p class="card-text">${pelicula.overview}</p>
-  </div>
-</div>
+            <div class="card my-1 card text-bg-dark " style="max-width: 540px;" >
+            <div class="row g-0">
+                <div class="col-md-5">
+                    <img src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-7">
+                    <div class="card-body">
+                      <h3 class="card-title" >${pelicula.title}</h3>
+                      <h6 class="card-subtitle mb-2">${pelicula.release_date}</h6>
+                      <p class="card-text">${overview}</p>
+                    </div>
+                  </div>
+              </div>
+            </div>
+      
             `);
         })
       })
